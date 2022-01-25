969e26e28ddba1bdec6369ccfc89dc65f97aa498
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2022-01-25 14:59:57 +0000

    Début de la BD

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,21 +2,21 @@
     	<div class="container py-3">
     		<div class="row no-gutters d-flex align-items-center align-items-stretch">
     			<div class="col-md-4 d-flex align-items-center py-4">
-    				<a class="navbar-brand" href="index.html">Negotiate <span>Consulting Agency</span></a>
+    				<a class="navbar-brand" href="index.html">ESI <span>Ecole Supérieure d'Industrie</span></a>
     			</div>
 	    		<div class="col-lg-8 d-block">
 		    		<div class="row d-flex">
 					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
 					    	<div class="icon d-flex justify-content-center align-items-center"><span class="ion-ios-paper-plane"></span></div>
 					    	<div class="text">
-					    		<span>Email</span>
+					    		<span>Adresse mail</span>
 						    	<span>youremail@email.com</span>
 						    </div>
 					    </div>
 					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
 					    	<div class="icon d-flex justify-content-center align-items-center"><span class="ion-ios-call"></span></div>
 						    <div class="text">
-						    	<span>Call</span>
+						    	<span>Contact</span>
 						    	<span>+123 523 5598</span>
 						    </div>
 					    </div>
@@ -42,7 +42,7 @@
 			    		</p>
 			    	</div>
 			    	<div class="col text-right">
-			    		<a href="#" class="btn-link">Request A Quote</a>
+			    		<a href="#" class="btn-link">Besoin d'aide ?</a>
 			    	</div>
 			    </div>
 		  	</div>
@@ -55,19 +55,19 @@
 	      </button>
 	      <form action="#" class="searchform order-lg-last">
           <div class="form-group d-flex">
-            <input type="text" class="form-control pl-3" placeholder="Search">
+            <input type="text" class="form-control pl-3" placeholder="Recherche">
             <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
           </div>
         </form>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav mr-auto">
-	        	<li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-	        	<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-	        	<li class="nav-item"><a href="team.html" class="nav-link">Team</a></li>
+	        	<li class="nav-item active"><a href="index.html" class="nav-link">Accueil</a></li>
+	        	<li class="nav-item"><a href="about.html" class="nav-link">A propos</a></li>
+	        	<li class="nav-item"><a href="team.html" class="nav-link">Enseignants</a></li>
 	        	<li class="nav-item"><a href="project.html" class="nav-link">Case Studies</a></li>
 	        	<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-	        	<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	        	<li class="nav-item"><a href="blog.html" class="nav-link">Actualités</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contacts</a></li>
 	        </ul>
 	      </div>
 	    </div>
